61907647143c8f5eab48b5178103bb5d796d34e5
Marcus <marconiushenry@gmail.com>
2021-06-12 19:23:22 -0500

    no significant changes, formatted spacing for clarity

--- serverless/get_coords.js
+++ serverless/get_coords.js
@@ -3,13 +3,16 @@ const fetch = require("node-fetch");
 const {WEATHER_API_KEY} = process.env;
 
 exports.handler = async (event, context)=> {
+
     const params = JSON.parse(event.body);
     const {text, units} = params;
+
     const regex = /^\d+$/g;
     const flag = regex.test(text) ? "zip" : "q";
-    const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${text}&units=${units}&appid=${WEATHER_API_KEY}`;
 
+    const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${text}&units=${units}&appid=${WEATHER_API_KEY}`;
     const encodedUrl = encodURI(url);
+    
     try{
         const dataStream = await fetch(encodedUrl);
         const jsonData = dataStream.json();
